12dfa8efd2c01c81b80dc4280679101bc7fd1dc2
Tammy Ritterskamp <tritterskamp@caleres.com>
2017-02-08 17:08:05 -0600

    setting up single page UI

---
+++ public/config.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var config = {
+    apiKey: "AIzaSyBiq9RIvYOMKbZqBW46HmuSNfnEgpM1qxI",
+    authDomain: "my-quickstart-67bc9.firebaseapp.com",
+    databaseURL: "https://my-quickstart-67bc9.firebaseio.com",
+    storageBucket: "my-quickstart-67bc9.appspot.com",
+    messagingSenderId: "995129079149"
+};
+firebase.initializeApp(config);
