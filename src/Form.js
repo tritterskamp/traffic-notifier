ee739cef014023d63d6842dd019e37991e39be21
Tammy Ritterskamp <tritterskamp@caleres.com>
2017-03-15 16:55:47 -0500

    converting admin to react app components

---
+++ src/Form.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  submitMessage(event) {
+    event.preventDefault();
+    const formValues = {
+      startAddress: this.startAddress.value,
+      endAddress: this.endAddress.value
+    }
+    this.props.addFormValues(formValues);
+    this.addressForm.reset();
+  }
+
+  render() {
+    return (
+        <form ref={(input) => this.addressForm = input} id="address-form" onSubmit={(e) => this.submitMessage(e)}>
+          <div className="form-group">
+            <input ref={(input) => this.startAddress = input} className="form-control" id="start-address" placeholder="Start address" />
+          </div>
+          <div className="form-group">
+            <input ref={(input) => this.endAddress = input} className="form-control" id="end-address" placeholder="End address" />
+          </div>
+          <button type="submit" className="btn btn-default">
+            Submit
+          </button>
+        </form>
+    )
+  }
+}
+export default Form;
