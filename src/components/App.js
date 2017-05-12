15d8abf0e1b9115926a2ec2abb48468c75ebec75
Tammy Ritterskamp <tritterskamp@caleres.com>
2017-05-12 16:53:31 -0500

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,16 @@
 import React, { Component } from 'react';
 import Login from './Login';
-import AdminPanel from './AdminPanel';
-import base from '../base';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.addFormValues = this.addFormValues.bind(this);
-    // getinitialstate
-    this.state = {
-      formValues: {},
-      isLoggedIn: false
-    };
-  }
-
-  componentWillMount() {
-    //this.ref = base.syncState(`${this.props.params.storeIf}/fishes`, {
-    this.ref = base.syncState(`form`, {
-      context: this,
-      state: 'formValues'
-    });
-  }
-
-  componentWillUnmount(){
-    base.removeBinding(this.ref);
-  }
-
-  addFormValues(formValue) {
-    // update our state
-    const formValues = {...this.state.formValues};
-    // add in new formValue
-    const timestamp = Date.now();
-    formValues[`formValue-${timestamp}`] = formValue;
-    // set state
-    this.setState({formValues: formValues})
-  }
 
   render() {
     return (
       <div id="container">
         <div className="container-fluid" id="main">
-          <Login isLoggedIn={this.state.isLoggedIn} />
-          <AdminPanel addFormValues={this.addFormValues} formValues={this.state.formValues} />
+          <Login />
+        </div>
+        <div className="container-fluid">
+          {this.props.children}
         </div>
       </div>
 
