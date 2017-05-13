d957c8a5fcc97393564d5182b81c3d185f7b1b3d
Tammy Ritterskamp <tritterskamp@gmail.com>
2017-05-13 15:35:17 -0500

    removing configs

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
-});
-
-export default base;
