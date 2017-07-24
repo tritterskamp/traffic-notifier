038916e3ff3456748ba298b20138c2ccbd3e99ac
Tammy Ritterskamp <tritterskamp@gmail.com>
2017-07-24 17:07:35 -0500

    Complete refactor of Traffic Notifier app. Considers this the start of Part II

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import App from './components/App';
-
-const Root = (props) => {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  )
-}
-
-render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
